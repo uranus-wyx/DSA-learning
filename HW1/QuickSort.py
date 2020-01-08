8590b35a065c9c825a7a75b0629f617c6c1ac4a1
Yuni <55570963+Yuni-wih@users.noreply.github.com>
2020-01-09 04:24:55 +0800

    Create QuickSort.py

---
+++ HW1/QuickSort.py
@@ -0,0 +1,15 @@
+def partition(array, left, right):
+    i = left-1
+    for j in range (left, right):
+        if array[j] <= array[right]:
+            i +=1
+            array[j] , array[i] = array[i] ,  array[j]
+    array[i+1],array[right] = array[right] , array[i+1]
+    return i+1 
+
+def quicksort (array, left, right):
+    
+    if left < right:
+        pivot = partition(array, left, right)
+        quicksort (array, left, pivot-1)
+        quicksort (array, pivot+1, right)
